67396d3a44d583849f9016daa52665f03e98dde7
Gian Libunao <gianlibunao@gmail.com>
2019-05-06 20:01:03 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -1,5 +1,22 @@
 require 'pry'
 
+x={
+     :winter => {
+       :christmas => ["Lights", "Wreath"],
+       :new_years => ["Party Hats"]
+     },
+     :summer => {
+       :fourth_of_july => ["Fireworks", "BBQ"]
+     },
+     :fall => {
+       :thanksgiving => ["Turkey"]
+     },
+     :spring => {
+       :memorial_day => ["BBQ"]
+     }
+   }
+
+
 def second_supply_for_fourth_of_july(holiday_hash)
   # given that holiday_hash looks like this:
   # {
@@ -18,30 +35,34 @@ def second_supply_for_fourth_of_july(holiday_hash)
   #   }
   # }
   # return the second element in the 4th of July array
+  holiday_hash[:summer][:fourth_of_july][1]
 end
 
 def add_supply_to_winter_holidays(holiday_hash, supply)
   # holiday_hash is identical to the one above
   # add the second argument, which is a supply, to BOTH the
   # Christmas AND the New Year's arrays
-
+  holiday_hash[:winter].each { |key,value| value << supply }
 end
 
 
 def add_supply_to_memorial_day(holiday_hash, supply)
   # again, holiday_hash is the same as the ones above
   # add the second argument to the memorial day array
+    holiday_hash[:spring].each { |key,value| value << supply }
 
 end
 
 def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_array)
   # code here
+  holiday_hash[season][holiday_name]=supply_array
+  holiday_hash
   # remember to return the updated hash
-
 end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
+  holiday_hash[:winter].values.flatten
 
 end
 
@@ -53,15 +74,35 @@ def all_supplies_in_holidays(holiday_hash)
   # Summer:
   #   Fourth Of July: Fireworks, BBQ
   # etc.
+  holiday_hash.each{|key,value| 
+  puts "#{key.capitalize}:"
+  value.each{|attri,data| 
+  if attri.to_s.split("_").length>1
+  puts "  #{attri.to_s.split("_").collect{|word| word.capitalize}.join(" ")}: #{data.join(", ")}"
+  else
+  puts "  #{attri.to_s.capitalize}: #{data.join(", ")}"
+  end
+  }
+  }
 
 end
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
-
+  rString =[]
+  holiday_hash.each{|key,value| 
+  value.each{|attri,data| 
+  if data.include?("BBQ")
+    rString << attri
+  end
+  }
+  }
+  rString
 end
 
+
+#binding.pry
 
 
 
